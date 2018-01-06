85e9d130cf22cc3ceb68ab1801cfbf3ce39794e9
Daniel Nass <dnlnss@gmail.com>
2018-01-06 17:54:27 -0500

    tweak about page

--- src/components/about.js
+++ src/components/about.js
@@ -2,12 +2,31 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 const About = () => (
-  <section className='cf center mt4 mb5 ph3-l ph4 f4 black-80 lh-copy flex flex-column flex-row-l items-center'>
-    <Helmet title='About' />
-    <div className='w-100 w-60-l mr4-l mb0-l mb3'>
-      <img className='db' src='/img/dn.jpg' alt='Daniel Nass' />
+  <section className="cf center mt4 mb5 ph3-l ph4 f4 black-80 lh-copy flex flex-column flex-row-l items-center">
+    <Helmet title="About" />
+    <div className="w-100 w-60-l mr4-l mb0-l mb3">
+      <img className="db" src="/img/daniel-nass.jpg" alt="Daniel Nass" />
     </div>
-    <div className='w-100 w-40-l ml4-l'><span className='b'>Daniel Nass</span> creates interactive and data-driven stories. Formerly the web editor of <a className='no-underline' target='_blank' rel='noopener noreferrer' href='http://www.phnompenhpost.com/'>The Phnom Penh Post</a>, he is now working as a freelancer. He lives in Brooklyn.</div>
+    <div className="w-100 w-40-l ml4-l">
+      <span className="b">Daniel Nass</span> creates interactive and data-driven
+      stories at{' '}
+      <a
+        className="no-underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.thetrace.org/author/dnass/"
+      >
+        The Trace
+      </a>. Previously, he was the web editor of{' '}
+      <a
+        className="no-underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.phnompenhpost.com/author/daniel-nass/85381"
+      >
+        The Phnom Penh Post
+      </a>. He lives in Brooklyn.
+    </div>
   </section>
 )
 
