169afa1523cd77c1704f02031fd8b0bb16c81a42
Daniel Nass <dnlnss@gmail.com>
2019-05-21 22:15:22 -0400

    Nav hover

--- src/components/navitem.js
+++ src/components/navitem.js
@@ -3,8 +3,8 @@ import { Link } from 'gatsby'
 
 export default ({ path, title }) => (
   <Link
-    activeClassName="bw2 fw6"
-    className="fw4 ph2 pv1 mh2 lh-solid no-underline black-50 ttu bb b--black-50"
+    activeClassName="bw2 fw7"
+    className="fw4 ph2 pv1 mh2 lh-solid no-underline black-50 ttu bb b--black-50 nav"
     to={path}
   >
     {title}
